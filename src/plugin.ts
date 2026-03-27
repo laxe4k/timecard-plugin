0d079bf5767441bd58bf1b5f1969df52294cc9ba
Laxe4k <laxe4k@hotmail.com>
2026-03-27 01:29:39 +0100

    Updates logger configuration and TypeScript settings
    Replaces `LogLevel` with a string value for logger level configuration, simplifying the code and adhering to updated API usage.
    
    Adds `node` types to TypeScript configuration for improved type checking and compatibility with Node.js modules.

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,9 +1,9 @@
-import streamDeck, { LogLevel } from "@elgato/streamdeck";
+import streamDeck from "@elgato/streamdeck";
 
 import { TimecardAction } from "./actions/timecard-action";
 
 // We can enable "trace" logging so that all messages between the Stream Deck, and the plugin are recorded. When storing sensitive information
-streamDeck.logger.setLevel(LogLevel.TRACE);
+streamDeck.logger.setLevel("trace");
 
 // Register the timecard action.
 streamDeck.actions.registerAction(new TimecardAction());
